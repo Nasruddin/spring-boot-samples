adc73f4b7304d021ada9fa58e85cfdaf725684c9
Nasruddin <nasruddin.java@gmail.com>
2021-08-17 01:14:03 +0530

    Forgot to add main runner class

---
+++ 2.x.x/basic-reactive-api/src/main/java/com/javatab/basicreactiveapi/BasicReactiveApiApplication.java
@@ -0,0 +1,13 @@
+package com.javatab.basicreactiveapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicReactiveApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicReactiveApiApplication.class, args);
+    }
+
+}
