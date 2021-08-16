f2497aa8a1bf0133ad1a0c08e38285876a018795
Nasruddin <nasruddin.java@gmail.com>
2021-08-17 01:16:21 +0530

    Committing remaining files

---
+++ 2.x.x/basic-reactive-api/src/test/java/com/javatab/basicreactiveapi/BasicReactiveApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javatab.basicreactiveapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicReactiveApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
